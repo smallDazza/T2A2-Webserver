47a7850d75228ef9823b6b6dbd4fb9a1cf6c2689
smallDazza <smalldazza@gmail.com>
2024-09-21 23:46:05 +1000

    continued backup

--- src/controllers/invite_controller.py
+++ src/controllers/invite_controller.py
@@ -6,6 +6,7 @@ from models.group import Group, GroupSchema
 from init import db
 
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from sqlalchemy.exc import IntegrityError
 
 
 invite_bp = Blueprint("invite", __name__, url_prefix="/invite")
@@ -23,32 +24,46 @@ def private_invite(outing_id, group_id):
 @invite_bp.route("/public", methods= ["POST"])
 @jwt_required()
 def public_invite():
-    body_data = InviteSchema().load(request.get_json())
-    stmt = db.select(Member).filter_by(member_id = get_jwt_identity())
-    member = db.session.scalar(stmt)
+    try:
+        body_data = request.get_json()
+        outing_id = body_data.get("outing_id")
+        group_id = body_data.get("group_id")
+        stmt = db.select(Member).filter_by(member_id = get_jwt_identity())
+        member = db.session.scalar(stmt)
 
-    pub_invite = Invite(
-        out_id = body_data.get("outing_id"),
-        fam_grp_id = body_data.get("group_id"),
-        member_id = member.member_id
-    )
-    if member.is_admin and member:
-        db.session.add(pub_invite)
-        db.session.commit()
-        
-        return {
-            "A public invitation for your this outing has been created.": invite_schema.dump(pub_invite)
-        }, 200
+        if not member:
+            return {"Error": "Member not found."}, 404
+
+        pub_invite = Invite(
+            out_id = outing_id,
+            fam_grp_id = group_id,
+            member_id = member.member_id,
+            message = body_data.get("message")
+        )
+        if member.is_admin:
+            db.session.add(pub_invite)
+            db.session.commit()
+            
+            return {
+                "A public invitation for your outing has been created.": {
+                    "The outing id": pub_invite.out_id,
+                    "Group id to invite": pub_invite.fam_grp_id,
+                    "Your message": pub_invite.message }
+            }, 200
+        else:
+            return {"Error": "You are not an admin and cannot create public invites."}, 401
+    except IntegrityError:
+        return {"Error": "The outing_id or group_id entered do not exist."}, 404
 
 @invite_bp.route("/private", methods= ["GET"])
 @jwt_required()
 def private_invites():
     stmt = db.select(Member).filter_by(member_id=get_jwt_identity())
     member = db.session.scalar(stmt)
-    stmt2 = db.select(Outing).where(public = False)
+    stmt2 = db.select(Outing).where(Outing.public == False)
     private = db.session.scalar(stmt2)
-    stmt3 = db.select(Invite).where(fam_grp_id = member.fam_group_id)
-    group_id = db.session.scalar(stmt3)
+    stmt3 = db.select(Invite).where(Invite.fam_grp_id == member.fam_group_id)
+    group_id = db.session.scalars(stmt3)
     if member and private and group_id:
         return {"Your private family group invites": invites_schema.dump(group_id)}, 200
     
@@ -57,17 +72,17 @@ def private_invites():
 def public_invites():
     stmt = db.select(Member).filter_by(member_id=get_jwt_identity())
     member = db.session.scalar(stmt)
-    stmt2 = db.select(Outing).where(public = True)
+    stmt2 = db.select(Outing).where(Outing.public == True)
     public = db.session.scalar(stmt2)
-    stmt3 = db.select(Invite).where(fam_grp_id = member.fam_group_id)
-    group_id = db.session.scalar(stmt3)
+    stmt3 = db.select(Invite).where(Invite.fam_grp_id == member.fam_group_id)
+    group_id = db.session.scalars(stmt3)
     if member and public and group_id:
         return {"Your public family group invites": invites_schema.dump(group_id)}, 200
     
 @invite_bp.route("/response", methods= ["POST"])
 @jwt_required()
 def invite_response():
-    body_data = InviteSchema().load(request.get_json())
+    body_data = request.get_json()
     if all(body_data.get(field) for field in ["outing_id", "family_group_id", "accept_invite"]):
 # Proceed if these 3 fields are present.
         pass
@@ -81,7 +96,7 @@ def invite_response():
         fam_grp_id = body_data.get("family_group_id"),
         accept_invite = body_data.get("accept_invite"),
         message = body_data.get("message"),
-        member_id = member.memberid
+        member_id = member.member_id
     )
     if member:
         db.session.add(accept)
